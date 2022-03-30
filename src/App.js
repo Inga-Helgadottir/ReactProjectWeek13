e880372246871737c5300597c4267fc0ff6612d6
GoodOldNin <71062853+GoodOldNin@users.noreply.github.com>
2022-03-30 11:28:46 +0200

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./styles/App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App"><h1></h1></div>;
+
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='Header'><h1>Pokemon To Do List</h1>
+    <button></button>
+    </div>
+  )
+}
+
+export default function Header();
