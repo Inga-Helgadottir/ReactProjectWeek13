33c9d0372430dd546bde840887093917e1dfa8e1
S. Inga Helgadottir <cph-sh557@cphbusiness.dk>
2022-03-30 11:40:38 +0200

    form look done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,36 @@
 import "./styles/App.css";
+import Form from "./components/Form";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div className="App"></div>;
-}
+  // const [showAddTask, setShowAddTask] = useState(false);
+  // const [tasks, setTasks] = useState([]);
 
+  // useEffect(() => {
+  //   const getTasks = async () => {
+  //     const tasksFromServer = await fetchTasks();
+  //     setTasks(tasksFromServer);
+  //   };
+  //   getTasks();
+  // }, []);
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      // setTasks(tasksFromServer);
+    };
+    // getTasks();
+  }, []);
+  const fetchTasks = async () => {
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon/");
+    const data = await res.json();
+    console.log(data);
+    return data;
+  };
+
+  return (
+    <div className="App">
+      <Form />
+    </div>
+  );
+}
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+import FormItem from "./FormItem";
+import "../styles/formStyle.css";
+
+const Form = () => {
+  return (
+    <form className="form">
+      <h2>Add task to list</h2>
+      <FormItem label="Task name" />
+      <FormItem label="Pokemon name" />
+      <input type="submit" value="Save Task" className="myButton" />
+    </form>
+  );
+};
+
+export default Form;
